2153a22740dbd3ccd0df83c37dacd530c5790b70
Renzo Bardalez <r.bardalez@outlook.com>
2025-02-28 00:42:51 -0500

    EntregaFinal-Bardalez

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,27 +1,27 @@
 import { useContext } from "react";
 import { CartContext } from "../context/CartContext";
-
+import "./Cart.css";
 function Cart () {
 
     const {cartItems, clearCart, finalizePurchase } =useContext(CartContext);
     return (
-        <div>
+        <div className="cart-container">
             <h2> Carrito de compras </h2>
             {cartItems.length === 0 ? (
                 <p> El carrito esá vacío </p>
             ) : (
                 cartItems.map((item) => (
                     <div key={item.id}>
-                        <p>
-                            {item.name} - Cantidad: {item.quantity} - Precio: {item.currency}
-                            {item.price}
+                        <p className="purchase-list">
+                            Item: {item.name} - Quantity: {item.quantity} - Price: ${item.currency}
+                            {item.price}.00
                         </p>
                         {/* <button onClick={()=> removeItem(item.id)}> Eliminar </button> */}
                     </div>
                 ))
             )}
-            <button onClick={finalizePurchase}> Finalizar compra </button>
-            <button onClick={clearCart}> Limpiar carrito </button>
+            <button className="finalize-purchase" onClick={finalizePurchase}> Finalizar compra </button>
+            <button className="clear-cart" onClick={clearCart}> Limpiar carrito </button>
         </div>
     );
 }
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -7,16 +7,16 @@ import { Link } from "react-router-dom";
 const CartWidget = () => {
     const { itemCount } = useContext(CartContext)
 return (
+    < Link to="/cart">
         <div className="cartWidget">
-            < Link to="/cart">
                 <div className="cartImage">
                     <img src ={cartIcon} alt="Ícono del carrito de compras"/>
                 </div>
                 <div className="cartCount">
                     <span> {itemCount} </span>
                 </div>
-            </Link>
         </div>
+    </Link>
 );
 };
 
